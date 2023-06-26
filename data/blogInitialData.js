d785aa1d18022b8e0cc79fd7fa894bcf083aae23
MaheswariKarthi <kmmaheswari24@gmail.com>
2023-06-26 09:54:00 +0530

    -Initail commit

---
+++ data/blogInitialData.js
@@ -0,0 +1,79 @@
+const blogInitialData = [
+    {
+        "blogTitle": "The Future of Artificial Intelligence",
+        "blogID": "01",
+        "blogArticle": "Exploring the potential of artificial intelligence and its impact on various industries. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.",
+        "blogAuthor": "John Smith",
+        "blogCategory": "Technology"
+        },
+        {
+        "blogTitle": "The Importance of Strength Training for Overall Fitness",
+        "blogID": "02",
+        "blogArticle": "Highlighting the benefits and importance of incorporating strength training into a fitness routine. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.",
+        "blogAuthor": "Benjamin Wilson",
+        "blogCategory": "Fitness"
+        },
+        {
+        "blogTitle": "Sustainable Fashion: Making Ethical Fashion Choices",
+        "blogID": "03",
+        "blogArticle": "Guidelines for making sustainable and ethical fashion choices to support the environment and fair trade practices. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.",
+        "blogAuthor": "Emma Thompson",
+        "blogCategory": "Fashion"
+        },
+        {
+        "blogTitle": "Top 10 Must-Watch Movies of the Year",
+        "blogID": "04",
+        "blogArticle": "A curated list of the top 10 must-watch movies released this year, covering various genres and captivating storytelling. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.",
+        "blogAuthor": "Lucas Moore",
+        "blogCategory": "Entertainment"
+        },
+        
+        {
+        "blogTitle": "Tips for Staying Motivated on Your Fitness Journey",
+        "blogID": "05",
+        "blogArticle": "Useful tips and strategies to stay motivated and overcome challenges while pursuing your fitness goals. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.",
+        "blogAuthor": "Daniel Adams",
+        "blogCategory": "Fitness"
+        },
+        {
+        "blogTitle": "2023 Fashion Trends: Colors, Patterns, and Styles",
+        "blogID": "06",
+        "blogArticle": "A comprehensive guide to the latest fashion trends in 2023, including popular colors, patterns, and styles. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.",
+        "blogAuthor": "Isabella Wilson",
+        "blogCategory": "Fashion"
+        },
+        {
+        "blogTitle": "The Impact of Social Media on Entertainment Industry",
+        "blogID": "07",
+        "blogArticle": "Examining how social media platforms have revolutionized the entertainment industry, influencing content creation and audience engagement. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.",
+        "blogAuthor": "William Thompson",
+        "blogCategory": "Entertainment"
+        },
+        {
+        "blogTitle": "The Evolution of Virtual Reality in Entertainment",
+        "blogID": "08",
+        "blogArticle": "Tracing the evolution of virtual reality technology and its impact on the entertainment industry, from gaming to immersive experiences. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.",
+        "blogAuthor": "Lucas Adams",
+        "blogCategory": "Entertainment"
+        },
+        {
+        "blogTitle": "The Applications of Blockchain Technology",
+        "blogID": "09",
+        "blogArticle": "Exploring the diverse applications of blockchain technology beyond cryptocurrencies, including supply chain management and digital identity. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.",
+        "blogAuthor": "Emma Johnson",
+        "blogCategory": "Technology"
+        },
+        {
+        "blogTitle": "Healthy Eating Tips for a Balanced Fitness Journey",
+        "blogID": "10",
+        "blogArticle": "Providing practical tips and guidance on adopting a healthy eating approach to support a balanced fitness journey. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.",
+        "blogAuthor": "Benjamin Wilson",
+        "blogCategory": "Fitness"
+        },
+        
+        
+        
+        
+]
+
+module.exports = blogInitialData
